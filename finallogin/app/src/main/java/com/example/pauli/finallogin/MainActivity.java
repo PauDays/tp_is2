899f8067632cea5cc21aa845c8db17dc5d233861
PauDays <pauleediaz@gmail.com>
2018-05-10 22:02:08 -0400

    loginv2

--- finallogin/app/src/main/java/com/example/pauli/finallogin/MainActivity.java
+++ finallogin/app/src/main/java/com/example/pauli/finallogin/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.pauli.finallogin;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +23,8 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 
@@ -26,10 +34,11 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 //implements OnClickListener
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     Button ok,back,exit;
     TextView result;
@@ -40,80 +49,107 @@ public class MainActivity extends Activity implements OnClickListener{
 
         // Login button clicked
         ok = findViewById(R.id.btn_login);
-        ok.setOnClickListener(this);
+        ok.setOnClickListener(this); ///////acaaaa fue el último cambio
 
         result =findViewById(R.id.lbl_result);
     }
 
-    public void postLoginData() {
-        // Create a new HttpClient and Post Header
-        HttpClient httpclient = new DefaultHttpClient();
-
-        /* login.php returns true if username and password is equal to saranga */
-        HttpPost httppost = new HttpPost("http://192.168.0.36:8084/scrumRestfinal/webresources/org.scrumrestfinal.entities.usuarios/login");
-        // new HttpPost("http://localhost:8084/scrumRestfinal/webresources/org.scrumrestfinal.entities.usuarios/log");
-
-        try {
-            // Add user name and password
-            EditText uname = findViewById(R.id.txt_username);
-            String username = uname.getText().toString();
-
-            EditText pword = findViewById(R.id.txt_password);
-            String password = pword.getText().toString();
-
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("usuario", username));
-            nameValuePairs.add(new BasicNameValuePair("contrasenha", password));
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-            // Execute HTTP Post Request
-            Log.w("LOGIN", "Execute HTTP Post Request");
-            HttpResponse response = httpclient.execute(httppost);
-
-            String str = inputStreamToString(response.getEntity().getContent()).toString();
-            //String str = inputStreamToString(response.getEntity().getContent()).toString();
-
-            Log.w("LOGIN", str);
-
-            if(str.toString().equalsIgnoreCase("true"))
-            {
-                Log.w("LOGIN", "TRUE");
-                result.setText("Login successful");
-            }else
-            {
-                Log.w("LOGIN", "FALSE");
-                result.setText(str);
-            }
-
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private StringBuilder inputStreamToString(InputStream is) {
-        String line = "";
-        StringBuilder total = new StringBuilder();
-        // Wrap a BufferedReader around the InputStream
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-        // Read response until the end
-        try {
-            while ((line = rd.readLine()) != null) {
-                total.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // Return full string
-        return total;
-    }
-
-    @Override
+       @Override
     public void onClick(View view) {
         if(view == ok){
-            postLoginData();
+            Login();
         }
     }
+    /*Metodo que sera llamado cuando el usuario haga click en el boton login*/
+    @SuppressLint("WrongConstant")
+    public void Login(){
+        //recupera los valores ingresados por el usuario
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText editTextUserName = (EditText) findViewById(R.id.txt_username);
+        EditText editTextPassword = (EditText) findViewById(R.id.txt_password);
+        String message;
+        //crea el objeto json que se enviara con la peticion
+        JSONObject loginParams = new JSONObject();
+
+        try {
+            loginParams.put("usuario", editTextUserName.getText().toString());
+            loginParams.put("contrasenha", editTextPassword.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            message = executePost("http://192.168.0.36:8084/scrumRestfinal/webresources/org.scrumrestfinal.entities.usuarios/login", loginParams.toString());
+            if (message.equals("")){
+                Toast.makeText(this,"FAILED LOGIN", 5).show();
+                return;
+            }
+            if(message.equalsIgnoreCase("false")){
+                Toast.makeText(this,"Los datos ingresados no coinciden", 25000).show();
+
+            }else{
+                //startActivity(intent);
+                Toast.makeText(this,"Login correcto", 25000).show();
+            }
+
+        }
+        catch(NullPointerException e){
+            Toast.makeText(this,"No se pudo conectar con el servidor", 5).show();
+        }
+
+    }
+    /*Metodo que realiza la conexion con el servidor y solicita el servicio post del login*/
+    @SuppressLint("WrongConstant")
+    public String executePost(String targetURL, String urlParameters) {
+        int timeout=15000;
+        URL url;
+        HttpURLConnection connection = null;
+        try {
+            //establece la conexion
+
+            url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(timeout);
+            connection.setReadTimeout(timeout);
+
+            //envia la peticion
+            DataOutputStream wr = new DataOutputStream(
+                    connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            // obtiene la respuesta
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+            }
+            rd.close();
+            return response.toString();
+
+        } catch (Exception e) {
+            Toast.makeText(this,"Error de conexión "+e.getMessage(), 10).show();
+            e.printStackTrace();
+        } finally {
+
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return null;
+    }
+
+
 
 }
