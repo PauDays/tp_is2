f88830f39c13142c9903b3d8d0d90ad3a85953f7
PauDays <pauleediaz@gmail.com>
2018-06-30 23:39:28 -0400

    correccion opciones

--- finallogin/app/src/main/java/com/example/pauli/finallogin/OpcionesUsuarios.java
+++ finallogin/app/src/main/java/com/example/pauli/finallogin/OpcionesUsuarios.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,31 +23,38 @@ public class OpcionesUsuarios extends AppCompatActivity {
 
 
         Intent nuevoIntent = getIntent();
-        final int idUsuario = nuevoIntent.getIntExtra("ID_USER", 0);
-        final String rolUsuario = nuevoIntent.getStringExtra("NOM_ROL");
-        System.out.println(rolUsuario);
+        final int idUsuario = nuevoIntent.getIntExtra("IDUSER", 0);
+        final String rolUsuario = nuevoIntent.getStringExtra("NOMROL");
+
+        //Log.d("myTag", "This is my message");
+       // Log.d("myTag", rolUsuario);
+      //  System.out.println(rolUsuario);
         final String personaJson=nuevoIntent.getStringExtra("Persona");
 
 
         btusuarios.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View V) {
+            public void onClick(View V)  {
+                try {
 
-                if (rolUsuario.equals("scrummaster")) {
-                    startActivity(new Intent(OpcionesUsuarios.this, windowSeeUser.class));
-                } else if (rolUsuario.equals("usuequipo")) {
+                    if (rolUsuario.equals("scrummaster")) {
+                        startActivity(new Intent(OpcionesUsuarios.this, windowSeeUser.class));
+                    } else if (rolUsuario.equals("usuequipo")) {
 
                         /* Intent firstIntent= new Intent(this, OpcionesUsuarios.class);
                     firstIntent.putExtra("ID_USER",resultRow.getId_usuario());*/
-                    Intent intentusuequipo = new Intent(OpcionesUsuarios.this, windowOnlyModify.class);
-                    intentusuequipo.putExtra("idUsuario", idUsuario);
-                    intentusuequipo.putExtra("personaJson",personaJson);
-                    startActivity(intentusuequipo);
+                        Intent intentusuequipo = new Intent(OpcionesUsuarios.this, windowOnlyModify.class);
+                        intentusuequipo.putExtra("idUsuario", idUsuario);
+                        intentusuequipo.putExtra("personaJson", personaJson);
+                        startActivity(intentusuequipo);
 
-                    // startActivity(new Intent(OpcionesUsuarios.this, windowOnlyModify.class));
+                        // startActivity(new Intent(OpcionesUsuarios.this, windowOnlyModify.class));
 
+                    }
+                }catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
         });
 
 
